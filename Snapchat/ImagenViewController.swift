ecc142328f93ab4b7615818afd1906a13711546b
Roque <>
2018-05-22 09:34:15 -0500

    Primer commit

---
+++ Snapchat/ImagenViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ImagenViewController.swift
+//  Snapchat
+//
+//  Created by Mac Tecsup on 16/05/18.
+//  Copyright © 2018 Tecsup. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ImagenViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var chooseContactButton: UIButton!
+    
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.image = image
+        imageView.backgroundColor = UIColor.clear
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.allowsEditing = false
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func chooseContactTapped(_ sender: Any) {
+        performSegue(withIdentifier: "chooseContactSegue", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let imagenesFolder = Storage.storage().reference().child("imagenes")
+        let imagenData = UIImagePNGRepresentation(imageView.image!)!
+        
+        
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ Snapchat/IniciarSesionViewController.swift
@@ -0,0 +1,43 @@
+//
+//  IniciarSesionViewController.swift
+//  Snapchat
+//
+//  Created by Mac Tecsup on 16/05/18.
+//  Copyright © 2018 Tecsup. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class IniciarSesionViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func iniciarSesionTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            print("Intentamos iniciar sesión")
+            if error != nil {
+                print("Tenemos el siguiente error\(String(describing: error))")
+                Auth.auth().createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!) { (user, error) in
+                    print("Intentamos crear un usuario")
+                    if error != nil {
+                        print("Tenemos el siguiente error\(String(describing: error))")
+                    }else{
+                        print("El usuario fue creado exitosamente")
+                        self.performSegue(withIdentifier: "iniciarSesionSegue", sender: nil)
+                    }
+                }
+            }else{
+                print("Inicio de sesión exitoso")
+                self.performSegue(withIdentifier: "iniciarSesionSegue", sender: nil)
+            }
+        }
+    }
+}
+
